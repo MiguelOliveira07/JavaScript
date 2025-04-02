d26aa2ce27b99dfb4ec02513dd35326c048b3a89
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-04-02 15:33:56 -0300

    3 aulas

---
+++ basico/operadores.js
@@ -0,0 +1,46 @@
+/*
+
+Todos os operadores
+
++
+-
+/
+*
+%
+++
+--
++=
+-=
+
+*/
+
+/*
+
+let num1= 0, num2=0,  num3=0
+
+num1=5
+num2=10
+
+res1=(num1+num2)
+res2=(num1-num2)
+res3=(num1/num2)
+res4=(num1*num2)
+res5=(num1%num2)
+res6=(num1**num2)
+
+console.log(res4)
+
+*/
+
+let num1= 0, num2=0,  num3=0
+
+num1=5
+num2=10
+
+//num1++
+//num2--
+//num1+=10
+num1*=1567
+num2-=5
+
+console.log(num1, num2)
---
+++ basico/tipos_variaveis.js
@@ -0,0 +1,36 @@
+"user strict"
+
+/* 
+
+function teste(){
+    if(true){
+       let nome="Miguel"
+       console.log("dentro de IF do teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+}
+teste()
+
+console.log("fora de teste: " +  nome)
+
+*/
+
+/* 
+
+let nome="Miguel"
+nome="CBF cursos"
+nome=10
+
+console.log("Fora de teste: " + nome)
+
+*/
+
+
+
+let nome="Miguel"
+nome="CBF Cursos"
+nome=10
+
+const curso='JavaScript'
+curso = "html"
+console.log(curso)
