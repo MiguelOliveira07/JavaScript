ac1fa29d44089768c9e833b040d2b2935ede6248
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-04-09 19:33:13 -0300

    Create funcoes_in_funcoes.js

---
+++ basico/funcoes_in_funcoes.js
@@ -0,0 +1,11 @@
+const soma = (...valores)=>{
+    const somar=val=>{
+        let res=0
+        for(v of val)
+            res+=v
+        return res  
+    }
+    somar(valores)
+}
+
+console.log(soma(1,2,3,4,5)) 
