a72de3b57dc90ca965276f96042b77bc401eb8df
Miguel <miguelalberto6110@gmail.com>
2025-04-18 17:33:39 -0300

    queryElement e getElement - finalizado

--- basico/DOM/querySelectorAll.js
+++ basico/DOM/querySelectorAll.js
@@ -1,11 +1,29 @@
-const todosCursos = [...document.getElementsByClassName('cursos')]
-const todosc1 = [...document.getElementsByClassName('c2')]
-const todosc2 = [...document.getElementsByClassName('c3')]
+// const divCursos = [...document.getElementsByTagName('div')]
+// const todosCursos = [...document.getElementsByClassName('cursos')]
+// const todosc1 = [...document.getElementsByClassName('c2')]
+// const todosc2 = [...document.getElementsByClassName('c3')]
+// const todosID = document.getElementById('c1')
 
-console.log(todosCursos)
-console.log(todosc1)
-console.log(todosc2)
 
-todosc1.map((el)=>{
-    el.classList.add('destaque')
-})
+const query_divs = [...document.querySelectorAll('div[class]')]
+const query_p_in_div= [...document.querySelectorAll('div>p')]
+const query_cursos = [...document.querySelectorAll('.cursos')]
+const query_c1 = [...document.querySelectorAll('.c1')]
+const query_c2 = [...document.querySelectorAll('.c2')]
+
+console.log(query_divs)
+console.log(query_p_in_div)
+console.log(query_cursos)
+console.log(query_c1)
+console.log(query_c2)
+
+
+// console.log(divCursos)
+// console.log(todosCursos)
+// console.log(todosc1)
+// console.log(todosc2)
+// console.log(todosID)
+
+// todosc1.map((el)=>{
+//     el.classList.add('destaque')
+// })
