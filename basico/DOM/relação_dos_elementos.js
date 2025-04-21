3922cb072579139c2aec9f2c2421e31e9807129b
Miguel <miguelalberto6110@gmail.com>
2025-04-21 15:37:12 -0300

    mais conteudo

--- basico/DOM/relação_dos_elementos.js
+++ basico/DOM/relação_dos_elementos.js
@@ -1,12 +1,22 @@
 const caixa_01 = document.querySelector("#caixa_01");
 const cursos = [...document.querySelectorAll(".curso")];
+const c1_02 = document.querySelector("#c1_02");
 
-console.log(caixa_01.hasChildNodes());
-console.log(cursos[1].hasChildNodes());
-console.log(cursos[1].childNodes);
+console.log(c1_02.parentNode.parentNode.parentNode.children[3]);
+
+// console.log(caixa_01.hasChildNodes());
+// console.log(cursos[1].hasChildNodes());
+// console.log(cursos[1].childNodes);
 
 // console.log(document.getRootNode()); //Nó Raiz
 
 // console.log(caixa_01.firstElementChild);
 // console.log(caixa_01.lastElementChild);
 // console.log(caixa_01.children);
+
+// console.log(
+//   caixa_01.children.length > 0 ? "Possui filhos" : "Não possui filhos"
+// );
+
+// console.log((caixa_01.children[1].innerHTML= "Teste"));
+
