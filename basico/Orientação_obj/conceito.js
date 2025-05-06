cfd95a5fec7515ddf2d450f142d2c0513c8181f3
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-05-06 18:15:12 -0300

    obj

--- basico/Orientação_obj/conceito.js
+++ basico/Orientação_obj/conceito.js
@@ -1,9 +1,57 @@
-class Pessoa {
-  constructor() {
-    this.nome = "Miguel";
+class Carro {
+  constructor(pnome, ptipo) {
+    this.nome = pnome;
+    if (ptipo ==1){
+        this.tipo = 'esportivo'
+        this.velmax = 300
+    } else if (ptipo ==2){
+      this.tipo = 'utilitário'
+      this.velmax = 110
+    } else if (ptipo == 3){
+      this.tipo = 'Passeio'
+      this.velmax = 110
+    } else if (ptipo ==4){
+      this.tipo = 'Militar'
+      this.velmax = 100
+    }
+  }
+  getNome(){
+      return this.nome
+    }
+  gettipo(){
+      return this.tipo
+    }
+  getVelcidade_max(){
+      return this.velmax
+    }
+  getInfo(){
+    return [this.nome, this.tipo, this.velmax]
+  }
+  setNome(nome){
+    this.nome = nome
+  }
+  info(){
+    console.log(`Nome: ${this.nome}`)
+    console.log(`Tipo: ${this.tipo}`)
+    console.log(`V.Max: ${this.velmax}`)
+    console.log("======================")
   }
 }
 
-let p1 =new Pessoa();
+let c1 =new Carro('meu_caro', 1);
+let c2 =new Carro('Anti_tanque', 4);
+let c3 =new Carro('Aluga_carro', 3);
 
-console.log(p1.nome)
+// console.log(c1.nome)
+// console.log(c1.tipo)
+// console.log(c1.velmax )
+
+// c1.info()
+// c2.info()
+// c3.info()
+
+// console.log(c1.getNome())
+console.log(c1.getInfo())
+ 
+c1.setNome('Super veloz')
+console.log(c1.getInfo())
---
+++ basico/orientação_obj/aula_02.js
@@ -0,0 +1,42 @@
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    
+  }
+  getNome(){
+      return this.nome
+    }
+  getidade(){
+      return this.idade
+  }
+  info(){
+    console.log(`Nome: ${this.nome}`)
+    console.log(`Tipo: ${this.idade}`)
+    console.log("======================")
+}
+}
+
+let pessoas = []
+
+const btn_adicionar = document.querySelector('#btn_adc');
+const res = document.querySelector('.rest');
+const Nome = document.querySelector('#nome');
+const Idade = document.querySelector('#idade');
+
+const addPessoas = () =>{
+    pessoas.map((p)=>{
+        const div = document.createElement("#f_nome")
+        div.setAttribute('class', 'pessoa')
+        div.innerHTML =` Nome : ${p.getNome}, idade: ${p.getidade}`
+    })
+}
+
+btn_adicionar.addEventListener('click', (evt) => {
+    const p = new Pessoa(Nome.value, Idade.value);
+    pessoas.push(p);
+    Nome.value = '';
+    Idade.value = '';
+    Nome.focus();
+    console.log(pessoas);
+});
