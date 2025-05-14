95c30f0b3a94a11403f0359d08ab922d08bd26d1
_Oliveira <137643185+MiguelOliveira07@users.noreply.github.com>
2025-05-14 15:52:20 -0300

    Update conversão_jason.js

--- basico/assuntos_diversos/conversão_jason.js
+++ basico/assuntos_diversos/conversão_jason.js
@@ -10,5 +10,7 @@ const pessoa ={
 }
 
 const s_json=JSON.stringify(pessoa) //converte nosso objeto literal em uma STRING JASON
+const o_json=JSON.parse(s_json) //converte nosso objeto literal em uma STRING JASON
 console.log(pessoa.nome)
 console.log(s_json)
+console.log(o_json)
