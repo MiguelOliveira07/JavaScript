da1d39a84a0ce4ae988b592b74d8fdcb0854f16e
Miguel <miguelalberto6110@gmail.com>
2025-05-25 14:33:23 -0300

    Update relogio_simples.js

--- basico/exercicios/relogio_simples.js
+++ basico/exercicios/relogio_simples.js
@@ -8,3 +8,23 @@ let mes_m = data.getMonth() < 10 ? "0" + data.getMonth() : data.getMonth();
 const data_r = dia_m + "/" + mes_m + "/" + data.getFullYear();
 
 data_div.innerHTML = data_r;
+
+const relogio = () => {
+  const data = new Date();
+  let hora = data.getHours();
+  hora = hora < 10 ? "0" + hora : hora;
+  let min = data.getMinutes();
+  min = min < 10 ? "0" + min : min;
+  let segundos = data.getSeconds();
+  segundos = segundos < 10 ? "0" + segundos : segundos;
+
+  //   console.log(hora);
+  //   console.log(min);
+  //   console.log(segundos);
+
+  hora_completa = hora + ":" + min + ":" + segundos;
+  relogio_div.innerHTML = hora_completa;
+};
+
+const intervalo = setInterval(relogio, 1000);
+// relogio();
