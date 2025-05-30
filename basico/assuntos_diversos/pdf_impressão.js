18710032ee8dad7e98bdaa651133ed5548e755b4
_Oliveira <137643185+MiguelOliveira07@users.noreply.github.com>
2025-05-30 18:10:49 -0300

    impressão e pdf no js

---
+++ basico/assuntos_diversos/pdf_impressão.js
@@ -0,0 +1,37 @@
+const btn_imprimir = document.querySelector('#btn_imprimir')
+
+const tabela = document.querySelector('#tabela')
+console.log(tabela.innerHTML)
+
+btn_imprimir.addEventListener('click', (evt) => {
+    const tabela = document.querySelector('#tabela').innerHTML
+
+    const win = window.open('', '', 'height=700; width=700')
+
+    const estilo = `
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        td, th {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+`;
+
+
+    win.document.write('<html>')
+    win.document.write('<head>')
+    win.document.write('<title> teste de impresão</title>')
+    win.document.write(estilo)
+    win.document.write('</head>')
+    win.document.write('<body>')
+    win.document.write(tabela)
+    win.document.write('</body>')
+    win.document.write('</html>')
+
+    win.print()
+})
