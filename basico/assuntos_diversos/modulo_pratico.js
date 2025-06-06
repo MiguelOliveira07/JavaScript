218e42e36037206eb57db8062fb96726163f3c6d
_Oliveira <137643185+MiguelOliveira07@users.noreply.github.com>
2025-06-06 17:13:12 -0300

    modulo

---
+++ basico/assuntos_diversos/modulo_pratico.js
@@ -0,0 +1,4 @@
+const cursos = ['JavaScript', 'Html', 'CSS', 'Python', 'C++', 'Java']
+const cores = ['azul', 'amarelo', 'vermelho', 'verde']
+
+export{cursos}
---
+++ basico/assuntos_diversos/modulos.js
@@ -0,0 +1,4 @@
+import { cursos } from "./modulo_pratico.js";
+
+console.log('ok, servidor rodando')
+console.log(cursos)
