44e58bb9d61350212906ad9f204de000146183d9
_Oliveira <137643185+MiguelOliveira07@users.noreply.github.com>
2025-06-06 18:09:25 -0300

    Update modulos_02.js

--- basico/assuntos_diversos/modulos_02.js
+++ basico/assuntos_diversos/modulos_02.js
@@ -1,5 +1,8 @@
-import getTodosCursos , { cursos, getCurso } from "./modulo_pratico.js";
+// import { cursos as c, getCurso as gc} from "./modulo_pratico.js";
+import getTodosCursos from "./modulo_pratico.js"
+import * as m_cursos from "./modulo_pratico.js"
 
-console.log(cursos)
-console.log(getTodosCursos)
-console.log(getCurso(2))
+console.log(m_cursos)
+// console.log(getTodosCursos)
+console.log(m_cursos.getCurso(0))
+console.log(m_cursos.default())
