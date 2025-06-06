7daa60f362528ed5ca7dc37215b8cd14db2bd75a
_Oliveira <137643185+MiguelOliveira07@users.noreply.github.com>
2025-06-06 17:59:31 -0300

    modulo 2

--- basico/assuntos_diversos/modulo_pratico.js
+++ basico/assuntos_diversos/modulo_pratico.js
@@ -1,4 +1,18 @@
 const cursos = ['JavaScript', 'Html', 'CSS', 'Python', 'C++', 'Java']
 const cores = ['azul', 'amarelo', 'vermelho', 'verde']
 
-export{cursos}
+// const getTodosCursos =()=>{
+//     return cursos
+// }
+
+export default function getTodosCursos (){
+    return cursos
+}
+
+function getCurso(curso){
+    return cursos[curso]
+}
+
+export{cursos, getCurso}
+
+// export default getTodosCursos
---
+++ basico/assuntos_diversos/modulos_02.js
@@ -0,0 +1,5 @@
+import getTodosCursos , { cursos, getCurso } from "./modulo_pratico.js";
+
+console.log(cursos)
+console.log(getTodosCursos)
+console.log(getCurso(2))
