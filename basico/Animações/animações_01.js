06068636cfc3d5760ce880d585f88be4449ce0fa
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-06-17 16:23:01 -0300

    update

--- basico/Animações/animações_01.js
+++ basico/Animações/animações_01.js
@@ -1,6 +1,7 @@
 const carro = document.querySelector("#carro");
 const seta_esquerda = document.querySelector("#btn_left");
 const seta_direita = document.querySelector("#btn_right");
+const parar = document.querySelector("#btn_stop");
 
 const init = () => {
   //   carro.style.position = "absolute";
@@ -8,15 +9,30 @@ const init = () => {
   carro.style = "position:absolute;left:0px";
 };
 
-seta_direita.addEventListener("click", () => {
-  let pos = parseInt(carro.style.left);
-  pos += 50;
-  carro.style = `position:absolute;left:${pos}px`;
-  console.log(pos);
+let anima = null;
+
+const mover_carro = (dir) => {
+  carro.style.left = parseInt(carro.style.left) + 5 * dir + "px";
+};
+
+parar.addEventListener("click", () => {
+  clearInterval(anima);
 });
+
 seta_esquerda.addEventListener("click", () => {
-    carro.style.left = parseInt(carro.style.left) - 50 + "px";
-    console.log(pos);
+  clearInterval(anima);
+  anima = setInterval(mover_carro, 20, -1);
+  //   console.log(pos);
+});
+
+seta_direita.addEventListener("click", () => {
+  //   let pos = parseInt(carro.style.left);
+  //   pos += 50;
+  //   carro.style = `position:absolute;left:${pos}px`;
+  //   console.log(pos);
+  clearInterval(anima);
+  anima = setInterval(mover_carro, 20, +1);
+  //   console.log(pos);
 });
 
 window.onload = init;
