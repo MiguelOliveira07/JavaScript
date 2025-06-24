a43561da0b134ef248027e3365131804c39209e7
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-06-24 15:58:15 -0300

    Update animações_01.js

--- basico/Animações/animações_01.js
+++ basico/Animações/animações_01.js
@@ -6,31 +6,48 @@ const parar = document.querySelector("#btn_stop");
 const init = () => {
   //   carro.style.position = "absolute";
   //   carro.style.left = '0px';
-  carro.style = "position:absolute;left:0px";
+  carro.style.position = "absolute";
+  carro.style.left = "0px";
+  carro.style.width = "100px";
+
+  tam_max = window.innerWidth - parseInt(carro.style.width);
 };
 
 let anima = null;
+let tam_max = null;
 
 const mover_carro = (dir) => {
-  carro.style.left = parseInt(carro.style.left) + 5 * dir + "px";
+  const pos = parseInt(carro.style.left); // posição atual do carro
+
+  if (dir > 0 && pos < tam_max) {
+    carro.style.left = (pos + 10) + "px";
+    anima = setTimeout(() => mover_carro(dir), 20);
+  } else if (dir < 0 && pos > 0) {
+    carro.style.left = (pos - 10) + "px";
+    anima = setTimeout(() => mover_carro(dir), 20);
+  } else{
+    clearTimeout(anima)
+    console.log('limite atingido.')
+  }
 };
 
+
 parar.addEventListener("click", () => {
-  clearTimeout(anima)
+  clearTimeout(anima);
   // clearInterval(anima);
 });
 
 seta_esquerda.addEventListener("click", () => {
-  clearTimeout(anima)
-  mover_carro(-1)
+  clearTimeout(anima);
+  mover_carro(-1);
   // clearInterval(anima);
   // anima = setInterval(mover_carro, 20, -1);
   //   console.log(pos);
 });
 
 seta_direita.addEventListener("click", () => {
-  clearTimeout(anima)
-  mover_carro(1)
+  clearTimeout(anima);
+  mover_carro(1);
   //   let pos = parseInt(carro.style.left);
   //   pos += 50;
   //   carro.style = `position:absolute;left:${pos}px`;
@@ -40,5 +57,8 @@ seta_direita.addEventListener("click", () => {
   //   console.log(pos);
 });
 
-window.onload = init;
-// window.addEventListener('load', init())
+// window.onload = init;
+window.addEventListener("load", init);
+window.addEventListener("resize", () => {
+  tam_max = window.innerWidth - parseInt(carro.style.width);
+});
