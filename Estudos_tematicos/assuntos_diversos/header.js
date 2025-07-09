ed940c4e33b2065ab0af896a5c5a1666243d3b56
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-07-09 16:50:18 -0300

    Header JS

---
+++ Estudos_tematicos/assuntos_diversos/header.js
@@ -0,0 +1,26 @@
+const head = document.head
+const body = document.body
+
+const estilo =  '<link rel="stylesheet" href="header.css">'
+head.innerHTML+=estilo
+
+const topo=document.createElement('div')
+topo.setAttribute('id','topo')
+topo.setAttribute('class','topo')
+
+body.prepend(topo)
+
+const logo=
+"<div id='logo' class='logo'>"+
+ "<img src='youtube.png' title='YouTube'/>"+
+"</div>"
+
+topo.innerHTML+=logo
+
+const login=
+    "<div id='login' class='login'"+
+        "<p id='matricula'>Matrícula:<span></span></p>"+
+        "<p id='nome'>Nome:<span></span></p>"
+    "</div>"
+
+topo.innerHTML+=login
