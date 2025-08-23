936d434d646ff0bd8e270e742c51a35bc188e806
Miguel <miguelalberto6110@gmail.com>
2025-08-23 07:57:38 -0300

    Push

--- Estudos_tematicos/Node.JS/Crud_01.js
+++ Estudos_tematicos/Node.JS/Crud_01.js
@@ -4,19 +4,31 @@ const server = express()
 server.use(express.json());
 
 let customers = [
-    { id : 1 , name: "GitHub", site:"https://github.com/MiguelOliveira07"},
-    { id : 2 , name: "YoutTube", site:"https://www.youtube.com/watch?v=5UgIxA32Q5I&t=1430s"},
-    { id : 3 , name: "UOL", site:"https://www.uol.com.br/"}
+    { id: 1, name: "GitHub", site: "https://github.com/MiguelOliveira07" },
+    { id: 2, name: "YoutTube", site: "https://www.youtube.com/watch?v=5UgIxA32Q5I&t=1430s" },
+    { id: 3, name: "UOL", site: "https://www.uol.com.br/" }
 ]
 
-server.get("/customers", (req, res)=>{
-     return res.json(customers)
+server.get("/customers", (req, res) => {
+    return res.json(customers)
 })
-server.get("/customers/:id", (req, res)=>{
+server.get("/customers/:id", (req, res) => {
     const id = parseInt(req.params.id)
-    const customer = customers.find(item=>item.id === id)
+    const customer = customers.find(item => item.id === id)
     const status = customer ? 200 : 404
-     return res.status(status).json(customer)
+    return res.status(status).json(customer)
+})
+
+
+server.post("/customers", (req, res) => {
+    const { nome, site } = req.body
+    const id = customers[customers.length - 1].id + 1
+
+    const newCustomer = { id, nome, site }
+
+    customers.push(newCustomer)
+
+    return res.status(201).json(newCustomer)
 })
 
 server.listen(3000);
