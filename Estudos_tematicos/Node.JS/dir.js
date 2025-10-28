0209f975e17d81fce06e91a93a8e1728960fba76
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-10-28 16:48:22 -0300

    Update dir.js

--- Estudos_tematicos/Node.JS/dir.js
+++ Estudos_tematicos/Node.JS/dir.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 if (!fs.existsSync('./new')) {
 
-    fs.mkdir('./new', (err) => {
+    fs.mkdir('./novo', (err) => {
         if (err) throw err;
         console.log('Directory created')
     })
