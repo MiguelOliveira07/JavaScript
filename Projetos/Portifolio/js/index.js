1198fe7e6d9e6fcb5a2de377e6681967c3ef1f36
Miguel <miguelalberto6110@gmail.com>
2025-12-03 21:40:29 -0300

    header resolvido

--- Projetos/Portifolio/js/index.js
+++ Projetos/Portifolio/js/index.js
@@ -1,1 +1,13 @@
- 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#head_js').insertAdjacentHTML('beforebegin', `
+        <header>
+            <h1>Miguel Oliveira</h1>
+            <ul id="menu">
+                <li class="menus" id="home">Home</li>
+                <li class="menus" id="educação">Educação</li>
+                <li class="menus" id="serviços">Serviços</li>
+                <li class="menus" id="contato">Contato</li>
+            </ul>
+        </header> 
+    `)
+})
