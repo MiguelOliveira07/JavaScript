4fef5226c59da9fb8466a6fdb1016166caa738bd
Miguel <miguelalberto6110@gmail.com>
2025-12-09 21:50:10 -0300

    update portifolio

--- Projetos/Portifolio/js/index.js
+++ Projetos/Portifolio/js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#page_body').insertAdjacentHTML('beforebegin', `
         <header>
-            <h1 class="menu-nome nome">Miguel Oliveira</h1>
+            <h1 class="menu-nome nome"><a href="../pages/index.html">Miguel Oliveira</a></h1>
             <ul id="menu">
                 <li class="menus" id="home"><a href="/pages/index.html">Home</a></li>
                 <li class="menus" id="serviços"><a href="../pages/serviços.html">Serviços</a></li>
