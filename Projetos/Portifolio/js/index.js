3927005d8eef8584ac9a179ef486df9815a3d637
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-12-10 17:29:32 -0300

    update

--- Projetos/Portifolio/js/index.js
+++ Projetos/Portifolio/js/index.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
             <h1 class="menu-nome nome"><a href="../pages/index.html">Miguel Oliveira</a></h1>
             <ul id="menu">
                 <li class="menus" id="home"><a href="/pages/index.html">Home</a></li>
-                <li class="menus" id="serviços"><a href="../pages/serviços.html">Serviços</a></li>
-                <li class="menus" id="educação"><a href="../pages/serviços.html">Formação</a></li>
+                <li class="menus" id="servicos"><a href="../pages/servicos.html">Serviços</a></li>
+                <li class="menus" id="educacao"><a href="../pages/educacao.html">Formação</a></li>
             </ul>
         </header> 
     `)
